201f0e6451d285b056848e6c7d07de869bbe943c
nolanjg2014-commits <nolanjg2014@gmail.com>
2026-05-04 20:25:43 -0700

    Add files via upload

---
+++ stock market project.py
@@ -0,0 +1,50 @@
+print("WELCOME TO MY MARKET WATCH PROJECT")
+print("  ")
+print("first we need a watchlist")
+print("ill make it for you")
+print("  ")
+up = 104.97
+down = 0.93
+sbux1 = up
+sbux2 = down
+print("sbux is up, "+str(sbux1)+" but is predicted to go down")
+print("it is down, "+str(sbux2)+" but still predicted to go down")
+print("im going to add 1 more stock to the market watch:")
+print("  ")
+advancing = 8.90
+decreasing = 3.38
+aapl1 = advancing 
+aapl2 = decreasing
+print("aapl is down, "+str(aapl2)+" but it is predicted to go down a little tiny bit")
+print("it is decreasing by, "+str(aapl2)+" but is still going down") 
+print("if you invest in these today here is what you will gain:")
+print("  ")
+gain_all_together = sbux1 
+print(gain_all_together)
+print("  ")
+print("but you will lose all together:")
+lose_all_together = sbux2 + aapl2
+print("  ")
+print(lose_all_together)
+print("  ")
+going_up = 392.56
+going_down = 0.17
+tsla1 = going_up
+tsla2 = going_down
+print("also tsla is going up, "+str(tsla1)+" and is doing the same thing as aapl")
+print("it is going down, "+str(tsla2))
+print("  ")
+print("ANALASIS TIME! YAY!")
+print("  ")
+print("if you invest in these today here is what you will gain alltogether:")
+what_you_will_gain = sbux1 + aapl1 + tsla1
+print("  ")
+print(what_you_will_gain)
+print(" ")
+print("but you will lose all together:")
+what_you_will_lose = sbux2 + aapl2 + tsla2
+print("  ")
+print(what_you_will_lose)
+print("thanks")
+print("for checking out my")
+print("project")
