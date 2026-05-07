5d11eea8a1c7c7eaf3676be0b3ac546ad4bdf7f6
nolanjg2014-commits <nolanjg2014@gmail.com>
2026-05-07 15:33:48 -0700

    Add files via upload

---
+++ extension.py
@@ -0,0 +1,12 @@
+print("this is an extention not the main code so if this is outdated it is because im not focasing on it")
+print("thanks for understanding")
+print("now lets get on to it")
+print("  ")
+tsla = 392.56
+aaple = -3.38
+sbux = 104.94
+print("this extention has more stocks but still the old ones")
+print("here is teh link for the google stocks:")
+print("https://www.google.com/finance/beta")
+oil_india = 384.4
+print("oil is up as of oild india ltd "+str(oil_india))
